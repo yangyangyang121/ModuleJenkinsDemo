66ccb05a37ab8f3480ab05517ecf972db421b14c
daniel.yang <daniel.yang@jinjiang.com>
2017-06-19 17:15:19 +0800

    m

---
+++ ModuleJenkinsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModuleJenkinsDemo
+//
+//  Created by yangdd on 2017/6/19.
+//  Copyright © 2017年 yangdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
